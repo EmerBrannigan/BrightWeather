ba1b39b2a5363742e66db5418f6d780e3f07258d
Emer Guilar <emerbrannigan@btinternet.com>
2026-01-18 16:48:36 +0000

    Fixing API Issues

--- BrightWeather/src/API/WeatherApi.js
+++ BrightWeather/src/API/WeatherApi.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.WEATHER_API_KEY;
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export async function getWeather(city, country = "GB") {
